b45199f6f0fb885cdd9a98ca9f8c580f7ad0ea4d
Andy <andreamarg0224@gmail.com>
2022-07-11 21:29:50 -0500

    Cambio de descuento a cupones

--- precios.js
+++ precios.js
@@ -1,15 +1,26 @@
 /* Primeros pasos
-
 const precioOriginal = 120;
 const descuento = 18;
-
 const porcentajePrecioConDescuento = 100 - descuento;
-
 const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
-
 /console.log ({
     precioOriginal, descuento, porcentajePrecioConDescuento, precioConDescuento,
 }); //Es una forma de usar el console.log para imprimir las variables que quieras. */
+
+const coupons = [
+    {
+        nombre: "Nuevo Ingreso",
+        descuento: 50,   
+    },
+    {
+        nombre: "Gran pedido",
+        descuento: 35,
+    },
+    {
+        nombre: "Regreso",
+        descuento: 20,
+    },
+];
 
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -17,15 +28,34 @@ function calcularPrecioConDescuento(precio, descuento){
 
     return precioConDescuento;
 }
+
+
+
 function priceDiscount(){
     const inputPrice = document.getElementById ("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById ("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById ("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue); 
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con Descuento es: $" + precioConDescuento;
+    const isCouponValid = function (coupon){
+        return coupon.nombre === couponValue;
+    }
+    
+    const userCoupon = coupons.find (isCouponValid);
+    
+    if (!userCoupon){
+        alert ("El cupón " + couponValue + "no es válido :c"); 
+    }else{
+        const descuento = userCoupon.descuento;
+        const precioConDescuento = calcularPrecioConDescuento (priceValue, descuento);
+        
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con Descuento es: $" + precioConDescuento;
+    }
 }
+
+
+
+//Para poder comentar una línea de código entera, puedes usar Ctrl + K y sin soltar, C.
